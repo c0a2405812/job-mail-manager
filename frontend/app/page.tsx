5cbb2e7bf9143511d2452419f79d182a945a920d
Okamoto <c0a2405812@edu.teu.ac.jp>
2026-06-15 08:41:46 +0900

    Category Change func

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,15 @@
+// Client Componentに変更
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+
+  const [selectCategory, setSelectedCategory]
+  = useState("すべて");
+
+
+  // メール本体
   const mails = [
     {
       subject: "〇〇株式会社 面接のご案内",
@@ -10,54 +21,61 @@ export default function Home() {
       subject: "大学からのお知らせ",
       sender: "university@example.com",
       date: "2026/06/01",
+      category:"大学",
     },
      {
       subject: "GitHub登録完了",
       sender: "github@example.com",
       date: "2026/05/31",
+      category:"個人",
     },
   ];
 
-  var selectCategory;
+  // メールフィルター
+  const filterMails = mails.filter((mail) => {
+    if(selectCategory === "すべて"){
+      return true;
+    }
 
-  function changeCategory(category: string){
-    selectCategory = category;
-  }
-
-  // function setSelectedCategory(category: string){
-  //   pass;
-  // }
+    return mail.category === selectCategory
+  });
 
   return (
-    <main>
-      <h1>Job Mail Manager</h1>
+    <html>
+      <body>
 
-      <button onClick={() => changeCategory("すべて")}>
-        すべて
-      </button>
+        <main>
+          <h1>Job Mail Manager</h1>
 
-      <button onClick={() => changeCategory("就活")}>
-        就活
-      </button>
-      
-      <button onClick={() => changeCategory("大学")}>
-        大学
-      </button>
-      
-      <button onClick={() => changeCategory("個人")}>
-        個人
-      </button>
+          <button onClick={() => setSelectedCategory("すべて")}>
+            すべて
+          </button>
+
+          <button onClick={() => setSelectedCategory("就活")}>
+            就活
+          </button>
+          
+          <button onClick={() => setSelectedCategory("大学")}>
+            大学
+          </button>
+          
+          <button onClick={() => setSelectedCategory("個人")}>
+            個人
+          </button>
 
 
-      {mails.map((mail) => (
-        <div key={mail.subject}>
-          <h3>{mail.subject}</h3>
-          <p>{mail.sender}</p>
-          <p>{mail.date}</p>
-          <p>{mail.category}</p>
-          <hr />
-        </div>
-      ))}
-    </main>
+          {filterMails.map((mail) => (
+            <div key={mail.subject}>
+              <h3>{mail.subject}</h3>
+              <p>{mail.sender}</p>
+              <p>{mail.date}</p>
+              <p>{mail.category}</p>
+              <hr />
+            </div>
+          ))}
+
+        </main>
+      </body>
+    </html>
   );
 }
